579fa4c1caaa239d0ecc5692649ab5ba3cdc2163
Jess Brunker <sloththing5@gmail.com>
2018-10-11 12:53:41 -0600

    Removed unneeded functions, added code for --

--- slackbot.py
+++ slackbot.py
@@ -15,77 +15,44 @@ bot_id = None
 
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = 'do'
-MENTION_REGEX = '^<@(|[WU].+?)>(.*)'
-PLUSPLUS_REGEX = '<@(|[WU].+?)>\s?[+]{2}'
+PLUSPLUS_REGEX = '<@(|[WU].+?)>\s?([+-]){2}'
 
 
-def parse_bot_commands(slack_events):
+def parse_messages(slack_events):
     '''
-    Parse a list of events c oming from the Slack RTM API to find bot
-    commands. If a bot command is found, this function returns a tuple
-    of command and channel. If it is not found, then this function
-    returns None, None.
+    Parse incoming messages looking for instances of ++ or --. Returns a
+    list of tuples of the form (username, plus/minus), and the channel in
+    which the mention appeared. Returns None, None if nothing is found.
     '''
     for event in slack_events:
         if event['type'] == 'message' and not 'subtype' in event:
-            '''
-            user_id, message = parse_direct_mention(event['text'])
-            if user_id == bot_id:
-                return message, event['channel']
-            '''
-            user_id = parse_direct_mention(event['text'])
-            print('user: {}'.format(user_id))
-            return user_id, event['channel']
+            pp_mentions = parse_plusplus_mentions(event['text'])
+            return pp_mentions, event['channel']
     return None, None
 
 
-def parse_direct_mention(message_text):
+def parse_plusplus_mentions(message_text):
     '''
-    Find a direct mention (at the beginning) in message text and returns
-    the user ID which was mentioned. If there is no direct mention,
-    returns None.
+    Search a given message and return all instances that match
+    PLUSPLUS_REGEX (text that matches @username++). Returns a tuple with
+    the username and whether it was a ++ or --.
     '''
-    print('message: {}'.format(message_text))
-    pp_matches = re.search(PLUSPLUS_REGEX, message_text)
-    return pp_matches.group(1) if pp_matches else None
-    '''
-    matches = re.search(MENTION_REGEX, message_text)
-    # first group contains username, second contains remaining message
-    if (matches):
-        return (matches.group(1), matches.group(2).strip()
-    else:
-        return (None, None)
-    '''
+    pp_matches = re.findall(PLUSPLUS_REGEX, message_text)
+    return pp_matches
 
 
-def handle_command(command, channel):
+def handle_plusplus(user_id, symbol, channel):
     '''
-    Execute bot command if the command is known
+    Increment a user's point value if the symbol is a '+', and decrement
+    the user's point value if the symbol is a '-'. Print out the user's
+    total after the operation is complete.
     '''
-    # Default response is help text for the user
-    default_response = 'Not sure what you mean. Try *{}*'.format(
-            EXAMPLE_COMMAND)
-
-    # Finds and executes the given command, filling in response
-    response = None
-    # put in more commands
-    if command.startswith(EXAMPLE_COMMAND):
-        response = 'write more code to do the thing'
-
-    # send response to the channel
+    text = '++' if symbol == '+' else '--'
     slack_client.api_call(
             'chat.postMessage',
             channel=channel,
-            text=response or default_response
-    )
-
-
-def handle_plusplus(user_id, channel):
-    slack_client.api_call(
-            'chat.postMessage',
-            channel=channel,
-            text='{} got a plusplus'.format(user_id)
+            # still need to connect to database, for now just print
+            text='<@{}> got a {}'.format(user_id, text)
     )
 
 
@@ -95,14 +62,10 @@ if __name__ == '__main__':
         # read bot's user ID by calling Web API method 'auth.test'
         bot_id = slack_client.api_call('auth.test')['user_id']
         while True:
-            '''
-            command, channel = parse_bot_commands(slack_client.rtm_read())
-            if command:
-                handle_command(command,channel)
-            '''
-            user_id, channel = parse_bot_commands(slack_client.rtm_read())
-            if user_id:
-                handle_plusplus(user_id, channel)
+            mentions, channel = parse_messages(slack_client.rtm_read())
+            if mentions:
+                for mention in mentions:
+                    handle_plusplus(mention[0], mention[1], channel)
             time.sleep(RTM_READ_DELAY)
     else:
         print('connection failed. exception trace printed above')
