e46a04f773ff334974ec864fbd68f150bda0846f
Jess Brunker <sloththing5@gmail.com>
2018-10-12 15:01:21 -0600

    Added functionality for bot commands, cleaned up functions

--- slackbot.py
+++ slackbot.py
@@ -6,18 +6,22 @@ import re
 import sqlite3
 
 from slackclient import SlackClient
+from slack_code import code as sc
 
 
 # instantiate Slack client
-slack_client = SlackClient(
-        'xoxb-453319474130-452584383648-nndKenwwf4UEsrPl1HRStZ5s')
+slack_client = SlackClient(sc)
+
 # bot's user ID in Slack: value assigned after startup
 bot_id = None
 
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+
+BOT_MENTION_REGEX = '^<@{}>\s([A-Za-z0-9\s]+)\s?(\S+)?$'
 USER_PP_REGEX = '<@(|[WU].+?)>\s?([+-]){2}' # finds @user++
-OTHER_PP_REGEX = '@([A-Za-z0-9]+[^>])\s?([+-]){2}' # finds @anything++
+OTHER_PP_REGEX = '@([A-Za-z0-9:#]+[^>])\s?([+-]){2}' # finds @anything++
+DB_FILE = 'scores.db'
 
 
 def parse_messages(slack_events):
@@ -29,24 +33,31 @@ def parse_messages(slack_events):
     '''
     for event in slack_events:
         if event['type'] == 'message' and not 'subtype' in event:
+            # check for '@bot {command}
+            command_matches = re.search(BOT_MENTION_REGEX, event['text'])
+            if command_matches and command_matches.group(1):
+                bot_command = command_matches.group(1)
+                parameters = command_matches.group(2)
+                handle_command(bot_command, parameters, event['channel'])
+                return None, None, None # do not allow ++ in a bot command
+
+            # check for @user++
             pp_mentions = re.findall(USER_PP_REGEX, event['text'])
             # don't want users to ++/-- themselves
             # we must shame them
             if self_gratification(event['user'], pp_mentions):
-                slack_client.api_call(
-                    'chat.postMessage',
-                    channel=event['channel'],
-                    text='<@{}> go ahead and pat yourself on the back'.format(
-                        event['user'])
-                    )
+                post_message('<@{}> just pat yourself on the back'.format(
+                    event['user']), event['channel'])
             # list all mentions not including the poster
             pp_mentions = set(filter(
-                lambda x: x[0] != event['user'], pp_mentions)) 
+                lambda x: x[0] != event['user'], pp_mentions))
 
+            # check for @nonuser++
             pp_others = re.findall(OTHER_PP_REGEX, event['text'])
             # want to exclude any entries that are actually usernames
             pp_others = set(filter(
                 lambda x: x not in pp_mentions, pp_others))
+
             return pp_mentions, pp_others, event['channel']
     return None, None, None
 
@@ -67,7 +78,7 @@ def handle_plusplus_mentions(mentions, channel):
     the user's point value if the symbol is a '-' and update the values in
     the database. Print out the user's total after the operation is complete.
     '''
-    conn = sqlite3.connect('scores.db')
+    conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     for mention in mentions:
         user_id, symbol = mention
@@ -90,20 +101,23 @@ def handle_plusplus_mentions(mentions, channel):
             # need to reverse the tuple to fit the query
             c.execute('''UPDATE UserScores SET Score = ?
                     WHERE User = ?''', (user_values[1], user_values[0]))
-        slack_client.api_call(
-            'chat.postMessage',
-            channel=channel,
-            text='<@{}> total points: {}'.format(
-                user_values[0], user_values[1])
-        )
+        post_message('<@{}> total points: {}'.format(
+            user_values[0], user_values[1]), channel)
     conn.commit()
 
 
 def handle_plusplus_others(pp_instances, channel):
-    conn = sqlite3.connect('scores.db')
+    '''
+    Increment the point value for arbitrary text if the symbol is a '+', and 
+    decrement the text's point value if the symbol is a '-' and update the
+    values in the database. Print out the text's total after the operation
+    is complete.
+    '''
+    conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     for instance in pp_instances:
         name, symbol = instance
+        name = name.strip()
         name_tup = (name,) # prevent SQL injections
         c.execute('''SELECT Name, Score FROM OtherScores
             WHERE Name = ?''', name_tup)
@@ -121,15 +135,150 @@ def handle_plusplus_others(pp_instances, channel):
                 score -= 1
             name_values = (name, score)
             # need to reverse the tuple to fit the query
-            c.execute('''UPDATE UserScores SET Score = ?
-                WHERE User = ?''', (name_values[1], name_values[0]))
-        slack_client.api_call(
-                'chat.postMessage',
-                channel=channel,
-                text='@{} total points: {}'.format(
-                    name_values[0], name_values[1])
-        )
+            c.execute('''UPDATE OtherScores SET Score = ?
+                WHERE Name = ?''', (name_values[1], name_values[0]))
+        post_message('@{} total points: {}'.format(
+            name_values[0], name_values[1]), channel) 
     conn.commit()
+
+
+def handle_command(cmd, params, channel):
+    '''
+    Given a bot command, call the appropriate function to manage the command.
+    Use the output to create a message to be posted to the channel.
+    '''
+    # split '@bot do some things' into [do] [some things]
+    command = cmd.lower().strip()
+    if params:
+        params = params.split()
+    message = ''
+
+    # Show the top 5 scores in both tables
+    if command in ['leaderboard', 'top']:
+        message = handle_lookup_users(5)
+        message += '\n\n'
+        message += handle_lookup_others(5)
+    # Show the score for a given user or other object
+    elif command == 'lookup':
+        if not params:
+            post_message('I don\'t know who you want me to lookup. Type "<@{0}> lookup @user" or "<@{0}> lookup @text"'.format(bot_id), channel)
+            return
+        subject = params[0]
+        result = handle_lookup_one(subject) 
+        message = '{} has {} points'.format(subject, result)
+    # Show the bottom 5 scores in both tables
+    elif command == 'bottom':
+        message = handle_lookup_users(-5)
+        message += '\n\n'
+        message += handle_lookup_others(-5)
+    # Show the help message
+    elif command in ['help', 'usage', 'commands', 'options']:
+        print_help(channel)
+        return
+    # Unknown command
+    else:
+        message = "I don't know what you want me to do. Try <@{}> help".format(
+                bot_id)
+    post_message(message, channel)
+
+
+def handle_lookup_users(amount):
+    '''
+    Find a given amount of users from the UserScores table. If amount < 0, we
+    need to find the lowest scores. Returns a string with the formatted data.
+    '''
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    query = 'SELECT * FROM UserScores ORDER BY Score'
+    # We need the top 5 scores
+    if amount > 0:
+        query += ' DESC'
+    message = '*Users leaderboard*'
+    c.execute(query)
+    results = c.fetchmany(amount)
+    count = 0
+    # Loop through the returned rows and format the message
+    while count < len(results) and count < 5:
+        message += '\n{} - <@{}>: {}'.format(
+                count+1, results[count][0], results[count][1])
+        count += 1
+    return message
+
+
+def handle_lookup_others(amount):
+    '''
+    Find a given amount of arbitrary text and their scores from the OtherScores
+    table. If amount < 0, we need to find the lowest scores. Returns a string
+    with the formatted data.
+    '''
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    query = 'SELECT * FROM OtherScores ORDER By Score'
+    # We need the top 5 scores
+    if amount > 0:
+        query += ' DESC'
+    message = '*Other leaderboard*'
+    c.execute(query)
+    results = c.fetchmany(amount)
+    count = 0
+    # Loop through the returned rows and format the message
+    while count < len(results) and count < 5:
+        message += '\n{} - @{}: {}'.format(
+                count+1, results[count][0], results[count][1])
+        count += 1
+    return message
+
+
+def handle_lookup_one(subject):
+    '''
+    Find the score for a given subject. First decides whether it is a user or
+    an "other", then queries the appropriate table. Returns the subject's score.
+    '''
+    table = ''
+    column = ''
+    # subject is a user
+    if re.match('<@[A-Za-z0-9]+>', subject):
+        table = 'UserScores'
+        column = 'User'
+        subject = re.sub('[<@>]', '', subject)
+    # subject is not a user
+    else:
+        table = 'OtherScores'
+        column = 'Name'
+        subject = re.sub('@', '', subject)
+    subject_param = (subject,)
+    query = 'SELECT * FROM {} WHERE {} = ?'.format(table, column)
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute(query, (subject_param))
+    result = c.fetchone()
+    if not result:
+        result = [subject, 0] # user is not in the database
+    return result[1] # only need the score
+
+
+def print_help(channel):
+    '''
+    Print a help message showing how to use the commands
+    '''
+    message = '''Here is a list of commands you can use with <@{0}> by typing <@{0}> [command]:
+
+    *leaderboard*: Show the top five scoring users and other objects
+    *bottom*: Show the bottom five scoring users and other objects
+    *lookup [user|object]*: Lookup the current score for the user or object
+        '''
+    post_message(message.format(bot_id), channel)
+
+
+def post_message(message, channel):
+    '''
+    Post a given message to the given channel
+    '''
+    slack_client.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text=message
+    )
 
 
 if __name__ == '__main__':
@@ -137,6 +286,8 @@ if __name__ == '__main__':
         print('bot connected and running')
         # read bot's user ID by calling Web API method 'auth.test'
         bot_id = slack_client.api_call('auth.test')['user_id']
+        # initialize regex with the bot id
+        BOT_MENTION_REGEX = BOT_MENTION_REGEX.format(bot_id)
         while True:
             mentions, others, channel = parse_messages(slack_client.rtm_read())
             if mentions:
